6c98bc18478b9750ea3429a633b5649e0a88b840
Gustav Nikolaj Olsen <gno@one.com>
2018-11-19 15:13:15 +0100

    add tests for deindent

--- test/deindent.spec.js
+++ test/deindent.spec.js
@@ -5,4 +5,80 @@ describe("deindent", () => {
   it("should be a function", () => {
     expect(deindent, "to be a function");
   });
+
+  it("should remove indentation from a string", () => {
+    const str = deindent`
+      deindent
+    `;
+
+    expect(str, "to equal", "deindent");
+  });
+
+  it("should remove indentation from a multiline string", () => {
+    const str = deindent`
+      foo bar
+      qux baz
+    `;
+
+    expect(str, "to equal", "foo bar\nqux baz");
+  });
+
+  it("should not remove additional indentation from a multiline string", () => {
+    const str = deindent`
+      foo bar
+        qux baz
+    `;
+
+    expect(str, "to equal", "foo bar\n  qux baz");
+  });
+
+  it("should not remove additional linebreaks from a multiline string", () => {
+    const str = deindent`
+      foo bar
+
+        qux baz
+    `;
+
+    expect(str, "to equal", "foo bar\n\n  qux baz");
+  });
+
+  it("should remove the right amount of indentation", () => {
+    const str = deindent`
+      foo
+        bar
+          qux
+      baz
+    `;
+
+    expect(str, "to equal", "foo\n  bar\n    qux\nbaz");
+  });
+
+  it("should remove leading lines with no non-whitespace contents", () => {
+    const str = deindent`
+
+      foo
+      bar
+    `;
+
+    expect(str, "to equal", "foo\nbar");
+  });
+
+  it("should still clean trailing whitespace when the first line has no indentation", () => {
+    const str = deindent`foo
+      bar
+    `;
+
+    expect(str, "to equal", "foo\n      bar");
+  });
+
+  it("should work with variables", () => {
+    const placeholder = "FOO BAR";
+    const str = deindent`
+      foo
+        ${placeholder}
+      bar
+    `;
+
+    expect(str, "to equal", "foo\n  FOO BAR\nbar");
+  });
 });
