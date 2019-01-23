aaf04049ae859d29401bcae8a690b8dd966445c1
Daniele Piccone <dpi@one.com>
2019-01-23 14:13:26 +0100

    collapse, handle string interpolation when it falls on line end

--- src/collapse.js
+++ src/collapse.js
@@ -1,15 +1,16 @@
 import zip from "./utils/zip";
 
 export default function collapse(strs, ...args) {
-  return zip(strs, ...args)
-    .map(str => {
-      return str
-        .split("\n")
-        .map(line => {
-          return line.replace(/^\s+/, "").replace(/\s+$/, "");
-        })
-        .filter(str => str.length > 0)
-        .join(" ");
-    })
-    .join("");
+  return (
+    zip(strs, ...args)
+      // get the full string
+      .join("")
+      // do a multiline trim
+      .replace(/^\s+/gm, "")
+      .replace(/\s+$/gm, "")
+      // join the lines, compatible with collapse
+      .replace(/\n/g, " ")
+      // trim the dangling line ends
+      .trim()
+  );
 }
